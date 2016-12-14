0a593c08cba28e5734d5a000e79608b5dbb697df
Oussama Mubarak <info@semiaddict.com>
2016-12-14 18:21:09 +0100

    fixed a minor CSS bug for the GuideSelector

--- dist/metaScore.player.js
+++ dist/metaScore.player.js
@@ -1,4 +1,4 @@
-/*! metaScore - v0.9.1 - 2016-12-06 - Oussama Mubarak */
+/*! metaScore - v0.9.1 - 2016-12-14 - Oussama Mubarak */
 ;(function (global) {
 "use strict";
 
@@ -132,7 +132,7 @@ var metaScore = {
      * @return {String} The revision identifier
      */
     getRevision: function(){
-        return "5076c0";
+        return "d3adf7";
     },
 
     /**
