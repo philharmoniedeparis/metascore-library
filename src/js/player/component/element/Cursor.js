a2704619b02f1b9ef84fc45665943fd7f54a7164
semiaddict <info@semiaddict.com>
2019-03-28 09:59:40 +0100

    feat: add circular cursors
    Add the possibility to create circular and elliptical cursors

--- src/js/player/component/element/Cursor.js
+++ src/js/player/component/element/Cursor.js
@@ -2,7 +2,7 @@ import Element from '../Element';
 import Dom from '../../../core/Dom';
 import Locale from '../../../core/Locale';
 import {toCSS} from '../../../core/utils/Color';
-import {map} from '../../../core/utils/Math';
+import {map, radians} from '../../../core/utils/Math';
 
 /**
  * A cursor element
@@ -23,6 +23,21 @@ export default class Cursor extends Element {
 
         return Object.assign({}, defaults, {
             'properties': Object.assign({}, defaults.properties, {
+                'border-radius': {
+                    'type': 'BorderRadius',
+                    'configs': {
+                        'label': Locale.t('player.component.Element.border-radius', 'Border radius')
+                    },
+                    'getter': function(skipDefault){
+                        return this.contents.css('border-radius', void 0, skipDefault);
+                    },
+                    'setter': function(value){
+                        this.contents.css('border-radius', value);
+                    },
+                    'applies': function(){
+                        return this.getPropertyValue('form') !== 'circular';
+                    }
+                },
                 'start-time': {
                     'type': 'Time',
                     'configs': {
@@ -53,6 +68,55 @@ export default class Cursor extends Element {
                         this.data('end-time', isNaN(value) ? null : value);
                     }
                 },
+                'form': {
+                    'type': 'Select',
+                    'configs': {
+                        'label': Locale.t('player.component.element.Cursor.form', 'Form'),
+                        'options': [
+                            {
+                                'value': 'linear',
+                                'text': Locale.t('player.component.element.Cursor.form.linear', 'Linear')
+                            },
+                            {
+                                'value': 'circular',
+                                'text': Locale.t('player.component.element.Cursor.form.circular', 'Circular')
+                            }
+                        ]
+                    },
+                    'getter': function(){
+                        const value = this.data('form');
+                        return value ? value : 'linear';
+                    },
+                    'setter': function(value){
+                        this.data('form', value);
+                    }
+                },
+                'mode': {
+                    'type': 'Select',
+                    'configs': {
+                        'label': Locale.t('player.component.element.Cursor.mode', 'Mode'),
+                        'options': [
+                            {
+                                'value': 'simple',
+                                'text': Locale.t('player.component.element.Cursor.mode.simple', 'Simple')
+                            },
+                            {
+                                'value': 'advanced',
+                                'text': Locale.t('player.component.element.Cursor.mode.advanced', 'Advanced')
+                            }
+                        ]
+                    },
+                    'getter': function(){
+                        const value = this.data('mode');
+                        return value ? value : 'simple';
+                    },
+                    'setter': function(value){
+                        this.data('mode', value);
+                    },
+                    'applies': function(){
+                        return this.getPropertyValue('form') === 'linear';
+                    }
+                },
                 'direction': {
                     'type': 'Select',
                     'configs': {
@@ -60,27 +124,90 @@ export default class Cursor extends Element {
                         'options': [
                             {
                                 'value': 'right',
-                                'text': Locale.t('player.component.element.Cursor.direction.right', 'Left > Right')
+                                'text': Locale.t('player.component.element.Cursor.direction.right', 'Left > Right'),
+                                'applies': function(){
+                                    return this.getPropertyValue('form') !== 'circular';
+                                }
                             },
                             {
                                 'value': 'left',
-                                'text': Locale.t('player.component.element.Cursor.direction.left', 'Right > Left')
+                                'text': Locale.t('player.component.element.Cursor.direction.left', 'Right > Left'),
+                                'applies': function(){
+                                    return this.getPropertyValue('form') !== 'circular';
+                                }
                             },
                             {
                                 'value': 'bottom',
-                                'text': Locale.t('player.component.element.Cursor.direction.bottom', 'Top > Bottom')
+                                'text': Locale.t('player.component.element.Cursor.direction.bottom', 'Top > Bottom'),
+                                'applies': function(){
+                                    return this.getPropertyValue('form') !== 'circular';
+                                }
                             },
                             {
                                 'value': 'top',
-                                'text': Locale.t('player.component.element.Cursor.direction.top', 'Bottom > Top')
+                                'text': Locale.t('player.component.element.Cursor.direction.top', 'Bottom > Top'),
+                                'applies': function(){
+                                    return this.getPropertyValue('form') !== 'circular';
+                                }
+                            },
+                            {
+                                'value': 'cw',
+                                'text': Locale.t('player.component.element.Cursor.direction.cw', 'Clockwise'),
+                                'applies': function(){
+                                    return this.getPropertyValue('form') === 'circular';
+                                }
+                            },
+                            {
+                                'value': 'ccw',
+                                'text': Locale.t('player.component.element.Cursor.direction.ccw', 'Counterclockwise'),
+                                'applies': function(){
+                                    return this.getPropertyValue('form') === 'circular';
+                                }
                             }
                         ]
                     },
                     'getter': function(){
-                        return this.data('direction');
+                        const value = this.data('direction');
+                        const form = this.data('form');
+                        return value ? value : (form === 'circular' ? 'cw' : 'right');
                     },
                     'setter': function(value){
                         this.data('direction', value);
+                    }
+                },
+                'start-angle': {
+                    'type': 'Number',
+                    'configs': {
+                        'label': Locale.t('player.component.element.Cursor.start-angle', 'Start angle'),
+                        'min': 0,
+                        'max': 360
+                    },
+                    'getter': function(){
+                        const value = parseInt(this.data('start-angle'), 10);
+                        return isNaN(value) ? 0 : value;
+                    },
+                    'setter': function(value){
+                        this.data('start-angle', value);
+                    },
+                    'applies': function(){
+                        return this.getPropertyValue('form') === 'circular';
+                    }
+                },
+                'loop-duration': {
+                    'type': 'Time',
+                    'configs': {
+                        'label': Locale.t('player.component.Cursor.loop-duration', 'Loop duration'),
+                        'clearButton': true
+                    },
+                    'getter': function(){
+                        const value = parseFloat(this.data('loop-duration'));
+                        return isNaN(value) ? null : value;
+                    },
+                    'setter': function(value){
+                        this.data('loop-duration', isNaN(value) ? null : value);
+                    },
+                    'applies': function(){
+                        return this.getPropertyValue('form') === 'circular';
                     }
                 },
                 'acceleration': {
@@ -179,7 +306,16 @@ export default class Cursor extends Element {
      * @private
      * @param {Event} evt The event object
      */
-    onPropChange(){
+    onPropChange(evt){
+
+        switch(evt.detail.property){
+            case 'width':
+            case 'height':
+            case 'border-width':
+                this.resizeCanvas();
+                break;
+        }
+
         this.draw();
     }
 
@@ -190,10 +326,20 @@ export default class Cursor extends Element {
      * @param {Event} evt The event object
      */
     onClick(evt){
-        const rect = this.get(0).getBoundingClientRect();
-        const time = this.getTimeFromPosition(evt.clientX - rect.left, evt.clientY - rect.top);
+        const form = this.getPropertyValue('form');
 
-        this.triggerEvent('time', {'element': this, 'value': time});
+        switch(form){
+            case 'circular':
+                break;
+
+            default: {
+                const rect = this.canvas.getBoundingClientRect();
+                const time = this.getTimeFromLinearPosition(evt.clientX - rect.left, evt.clientY - rect.top);
+
+                this.triggerEvent('time', {'element': this, 'value': time});
+                break;
+            }
+        }
     }
 
     /**
@@ -209,56 +355,153 @@ export default class Cursor extends Element {
     }
 
     /**
-     * Update the <canvas> size
+     * Readjust the canvas's size
      *
+     * @private
      * @return {this}
      */
-    draw(){
+    resizeCanvas(){
         const width = this.contents.get(0).clientWidth;
         const height = this.contents.get(0).clientHeight;
-
-        const direction = this.getPropertyValue('direction');
-        const cursor_width = this.getPropertyValue('cursor-width');
-        const cursor_color = this.getPropertyValue('cursor-color');
-
-        const pos = this.getPositionFromTime(this.current_time);
-
-        let w = width;
-        let h = height;
-
-        switch(direction){
-            case 'bottom':
-            case 'top':
-                h = cursor_width;
-                break;
-
-            case 'left':
-            default:
-                w = cursor_width;
-        }
 
         // Resize the canvas.
         this.canvas.width = width;
         this.canvas.height = height;
 
-        // Clear the canvas.
-        this.context.clearRect(0, 0, width, height);
+        return this;
+    }
 
-        // Draw the cursor line.
-        this.context.fillStyle = cursor_color;
-        this.context.fillRect(pos.x, pos.y, w, h);
+    /**
+     * Update the <canvas> size
+     *
+     * @return {this}
+     */
+    draw(){
+        const form = this.getPropertyValue('form');
+
+        if(this.canvas.width === 0 || this.canvas.height === 0){
+            this.resizeCanvas();
+        }
+
+        // Clear the canvas.
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        switch(form){
+            case 'circular':
+                this.drawCircCursor();
+                break;
+
+            default:
+                this.drawRectCursor();
+                break;
+        }
 
         return this;
     }
 
     /**
-     * Helper function to get a position on the cursor corresponding to a media time
+     * Draw a rectangular cursor
+     *
+     * @return {this}
+     */
+    drawRectCursor(){
+        const width = this.canvas.width;
+        const height = this.canvas.height;
+
+        const direction = this.getPropertyValue('direction');
+        const cursor_width = this.getPropertyValue('cursor-width');
+        const cursor_color = this.getPropertyValue('cursor-color');
+
+        const pos_1 = this.getLinearPositionFromTime(this.current_time);
+        const pos_2 = {
+            x: pos_1.x,
+            y: pos_1.y
+        };
+
+        switch(direction){
+            case 'bottom':
+            case 'top':
+                pos_2.x = width;
+                break;
+
+            case 'left':
+            default:
+                pos_2.y = height;
+        }
+
+        // Draw the cursor line.
+        this.context.save();
+        this.context.translate(0.5, 0.5);
+        this.context.beginPath();
+        this.context.moveTo(pos_1.x, pos_1.y);
+        this.context.lineTo(pos_2.x, pos_2.y);
+        this.context.lineWidth = cursor_width;
+        this.context.strokeStyle = cursor_color;
+        this.context.stroke();
+        this.context.closePath();
+        this.context.restore();
+
+        return this;
+    }
+
+    /**
+     * Draw a circular cursor
+     *
+     * @return {this}
+     */
+    drawCircCursor(){
+        const width = this.canvas.width;
+        const height = this.canvas.height;
+
+        const border_width = this.getPropertyValue('border-width');
+        const start_time = this.getPropertyValue('start-time');
+        const end_time = this.getPropertyValue('end-time');
+        const direction = this.getPropertyValue('direction');
+        const start_angle = radians(this.getPropertyValue('start-angle'));
+        const loop_duration = this.getPropertyValue('loop-duration') || end_time - start_time;
+        const cursor_width = this.getPropertyValue('cursor-width');
+        const cursor_color = this.getPropertyValue('cursor-color');
+
+        let angle = start_angle;
+        angle += map(this.current_time - start_time, 0, loop_duration, 0, 2 * Math.PI);
+        if(direction === 'ccw'){
+            angle *= -1;
+        }
+
+        const centre = {
+            x: width / 2,
+            y: height / 2
+        };
+
+        const point = {
+            x: centre.x - ((width / 2 + border_width) * Math.cos(angle)),
+            y: centre.y - ((height / 2 + border_width) * Math.sin(angle))
+        };
+
+        // Draw the cursor line.
+        this.context.save();
+        this.context.translate(0.5, 0.5);
+        this.context.beginPath();
+        this.context.moveTo(centre.x, centre.y);
+        this.context.lineTo(point.x, point.y);
+        this.context.lineCap = "round";
+        this.context.lineWidth = cursor_width;
+        this.context.strokeStyle = cursor_color;
+        this.context.stroke();
+        this.context.closePath();
+        this.context.restore();
+
+        return this;
+    }
+
+    /**
+     * Helper function to get a position on a linear cursor corresponding to a media time
      *
      * @private
      */
-    getPositionFromTime(time){
-        const width = this.contents.get(0).clientWidth;
-        const height = this.contents.get(0).clientHeight;
+    getLinearPositionFromTime(time){
+        const width = this.canvas.width;
+        const height = this.canvas.height;
 
         const start_time = this.getPropertyValue('start-time');
         const end_time = this.getPropertyValue('end-time');
@@ -271,32 +514,28 @@ export default class Cursor extends Element {
         };
 
         switch(direction){
-            case 'top':{
+            case 'top':
                 pos.y = map(time, start_time, end_time, 0, height);
                 pos.y = height - Math.pow(pos.y, acceleration);
                 pos.y = Math.round(pos.y);
                 break;
-            }
 
-            case 'bottom':{
+            case 'bottom':
                 pos.y = map(time, start_time, end_time, 0, height);
                 pos.y = Math.pow(pos.y, acceleration);
                 pos.y = Math.round(pos.y);
                 break;
-            }
 
-            case 'left':{
+            case 'left':
                 pos.x = map(time, start_time, end_time, 0, width);
                 pos.x = width - Math.pow(pos.x, acceleration);
                 pos.x = Math.round(pos.x);
                 break;
-            }
 
-            default:{
+            default:
                 pos.x = map(time, start_time, end_time, 0, width);
                 pos.x = Math.pow(pos.x, acceleration);
                 pos.x = Math.round(pos.x);
-            }
         }
 
         return pos;
@@ -307,9 +546,9 @@ export default class Cursor extends Element {
      *
      * @private
      */
-    getTimeFromPosition(x, y){
-        const width = this.contents.get(0).clientWidth;
-        const height = this.contents.get(0).clientHeight;
+    getTimeFromLinearPosition(x, y){
+        const width = this.canvas.width;
+        const height = this.canvas.height;
 
         const start_time = this.getPropertyValue('start-time');
         const end_time = this.getPropertyValue('end-time');
