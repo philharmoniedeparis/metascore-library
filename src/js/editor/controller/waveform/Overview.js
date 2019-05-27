fc41d401d900cd425efbeda12a531508fff5fdfe
semiaddict <info@semiaddict.com>
2019-05-27 15:41:52 -0400

    fix: improve some mouseove listeners
    Calculate the mouse offset using the correct element in the waveform overview and buffer indicator

--- src/js/editor/controller/waveform/Overview.js
+++ src/js/editor/controller/waveform/Overview.js
@@ -286,7 +286,7 @@ export default class Overview extends Dom {
             return;
         }
 
-        const offset = evt.target.getBoundingClientRect();
+        const offset = this.get(0).getBoundingClientRect();
         const x = evt.pageX - offset.left;
         const time = this.getTimeAt(x);
 
