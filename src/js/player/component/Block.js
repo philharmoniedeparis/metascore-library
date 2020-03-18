philharmoniedeparis/metascore-library

commit 00f57cc1b3b69050e902c3cd5f6cbb23723e6243

fix: fix block pagination bug
Fix a bug in Block.setActivePage that prevented switching back to page 0 due to its start-time propoerty being null.

Closes: #374

src/js/player/component/Block.js
@@ -382,16 +382,7 @@ export default class Block extends Component {
 
             if(_page instanceof Page && _page !== previous){
                 const start_time = _page.getPropertyValue('start-time');
-                if(start_time !== null){
-                    MasterClock.setTime(start_time);
-                }
-                else{
-                    if(previous){
-                        previous.deactivate();
-                    }
-
-                    _page.activate();
-                }
+                MasterClock.setTime(start_time || 0);
             }
         }
 

commit c49849fded3b80d174fa61826dd3bfe801262f8d

fix: fix non-synced block pagination
Fix a bug introduced by commit 00f57cc, which prevents the pagination of non-synchronized blocs from functioning

src/js/player/component/Block.js
@@ -382,7 +382,17 @@ export default class Block extends Component {
 
             if(_page instanceof Page && _page !== previous){
                 const start_time = _page.getPropertyValue('start-time');
-                MasterClock.setTime(start_time || 0);
+                const end_time = _page.getPropertyValue('end-time');
+                if(start_time !== null || end_time !== null){
+                    MasterClock.setTime(start_time || 0);
+                }
+                else{
+                    if(previous){
+                        previous.deactivate();
+                    }
+
+                    _page.activate();
+                }
             }
         }
 