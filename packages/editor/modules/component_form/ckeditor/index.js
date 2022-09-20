88a0dd44dd384923e75e2c393e8df5476466b8d4
semiaddict <info@semiaddict.com>
2022-09-20 17:53:58 +0200

    feat: add more heading options in CKEditor

--- packages/editor/modules/component_form/ckeditor/index.js
+++ packages/editor/modules/component_form/ckeditor/index.js
@@ -113,6 +113,20 @@ function getConfig({ language = "fr", extraFonts = [] } = {}) {
       ],
       shouldNotGroupWhenFull: true,
     },
+    heading: {
+      options: [
+        { model: "paragraph", title: "Paragraph" },
+        { model: "heading1", view: "h1", title: "Heading 1" },
+        { model: "heading2", view: "h2", title: "Heading 2" },
+        { model: "heading3", view: "h3", title: "Heading 3" },
+        { model: "heading4", view: "h4", title: "Heading 4" },
+        { model: "heading5", view: "h5", title: "Heading 5" },
+        { model: "heading6", view: "h6", title: "Heading 6" },
+        { model: "div", view: "div", title: "Division" },
+        { model: "pre", view: "pre", title: "Preformatted" },
+        { model: "address", view: "address", title: "Address" },
+      ],
+    },
     fontFamily: {
       options: [
         "default",
