fd72448f06227f165ebb7b35c62a260361c988d1
semiaddict <info@semiaddict.com>
2022-11-30 20:31:59 +0100

    feat: allow adding new i18n messages outside of vue components

--- packages/core/services/i18n/index.js
+++ packages/core/services/i18n/index.js
@@ -10,5 +10,21 @@ export function init(configs) {
 export default instance;
 
 export function t(...args) {
+  if (!instance) {
+    throw new Error("Cannot use 't' before initializing the i18n service.");
+  }
+
   return instance.global.t(...args);
+}
+
+export function addMessages(messages) {
+  if (!instance) {
+    throw new Error(
+      "Cannot add i18n messages before initializing the i18n service."
+    );
+  }
+
+  Object.entries(messages).forEach(([locale, value]) => {
+    instance.global.mergeLocaleMessage(locale, value);
+  });
 }
