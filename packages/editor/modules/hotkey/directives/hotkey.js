593bbe80a7fb77b465e53f90d9415925257dcdf7
semiaddict <info@semiaddict.com>
2023-03-15 17:46:47 +0100

    fix: fix hotkey binding update

--- packages/editor/modules/hotkey/directives/hotkey.js
+++ packages/editor/modules/hotkey/directives/hotkey.js
@@ -3,58 +3,58 @@ import useStore from "../store";
 
 export default {
   mounted(el, binding) {
-    const { group, keys } = binding.value;
-    if (!group || !keys) {
-      plugin.directive.mounted(el, binding);
-      return;
-    }
-
     const store = useStore();
-    const value = {};
+    let { value } = binding;
 
-    Object.entries(keys).forEach(([k, { handler, description }]) => {
-      store.addCombination(group, k, description);
-      value[k] = handler;
-    });
+    const { group, keys } = value;
+    if (group && keys) {
+      value = {};
+
+      Object.entries(keys).forEach(([k, { handler, description }]) => {
+        store.addCombination(group, k, description);
+        value[k] = handler;
+      });
+    }
 
     plugin.directive.mounted(el, { ...binding, value });
   },
   updated(el, binding) {
     const store = useStore();
+    let { value, oldValue } = binding;
 
-    if (binding.oldValue) {
-      const { group: oldGroup, keys: oldKeys } = binding.oldValue;
+    if (oldValue) {
+      const { group: oldGroup, keys: oldKeys } = oldValue;
       if (oldGroup && oldKeys) {
+        oldValue = {};
         Object.keys(oldKeys).forEach((k) => {
           store.removeCombination(oldGroup, k);
         });
       }
     }
 
-    const { group, keys } = binding.value;
-    if (!group || !keys) {
-      plugin.directive.mounted(el, binding);
-      return;
+    const { group, keys } = value;
+    if (group && keys) {
+      value = {};
+      Object.entries(keys).forEach(([k, { handler, description }]) => {
+        store.addCombination(group, k, description);
+        value[k] = handler;
+      });
     }
 
-    const value = {};
-
-    Object.entries(keys).forEach(([k, { handler, description }]) => {
-      store.addCombination(group, k, description);
-      value[k] = handler;
-    });
-
-    plugin.directive.updated(el, { ...binding, value });
+    plugin.directive.updated(el, { ...binding, value, oldValue });
   },
   beforeUnmount(el, binding) {
     const store = useStore();
-    const { group, keys } = binding.value;
+    let { value } = binding;
+
+    const { group, keys } = value;
     if (group && keys) {
+      value = {};
       Object.keys(binding.value).forEach((k) => {
         store.removeCombination(group, k);
       });
     }
 
-    plugin.directive.beforeUnmount(el, binding);
+    plugin.directive.beforeUnmount(el, { ...binding, value });
   },
 };
