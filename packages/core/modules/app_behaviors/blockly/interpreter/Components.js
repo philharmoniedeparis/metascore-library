c3b0e8ec1940d522cb5f917bb9f36afc20c163c6
semiaddict <info@semiaddict.com>
2024-12-09 14:52:50 +0100

    fix: remove stale _onScenarioChange reference from Components behavior interpreter

--- packages/core/modules/app_behaviors/blockly/interpreter/Components.js
+++ packages/core/modules/app_behaviors/blockly/interpreter/Components.js
@@ -1,6 +1,5 @@
 import { javascriptGenerator as JavaScript } from "blockly/javascript";
 import { useModule } from "@core/services/module-manager";
-import { watch } from "vue";
 import AbstractInterpreter from "./AbstractInterpreter";
 
 const SET_PROPERTY_OVERRIDES_KEY = "app_behaviors:set_property";
@@ -12,17 +11,9 @@ export default class Components extends AbstractInterpreter {
 
     // Ensure context name does not conflict with variable names.
     JavaScript.addReservedWords("Components");
-
-    this._unwatchActiveScenario = null;
   }
 
   get context() {
-    const { activeScenario } = useModule("app_components");
-    this._unwatchActiveScenario = watch(
-      activeScenario,
-      this._onScenarioChange.bind(this)
-    );
-
     return {
       Components: {
         getProperty: (component, property) => {
@@ -94,13 +85,5 @@ export default class Components extends AbstractInterpreter {
         },
       },
     };
-  }
-
-  reset() {
-    // Remove watcher.
-    if (this._unwatchActiveScenario) {
-      this._unwatchActiveScenario();
-      this._unwatchActiveScenario = null;
-    }
   }
 }
